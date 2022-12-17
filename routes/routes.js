ab6e351e4aaa36d38576d1a2cd5913898a8253c5
carlosreyes <carlosreyes8436@gmail.com>
2022-12-16 23:04:15 -0600

    rename userController document

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,23 @@
 const { getMovie, addMovie, getMovieById, editMovie, deleteMovie } = require('../controllers/movieControllers')
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser } = require('../controllers/authController')
 const { handleError, pageNotFound } = require('../middlewares/errorMiddlewares')
 
 
 const routes = (app) => {
 
+    //-------Home----------------
     app.get('/', (req, res) => res.send('<h1>Bienvenido</h1>'))
+    //------Login----------
     app.post('/api/register', registerUser)
     app.post('/api/login', loginUser)
+    //------Usuarios-------------
+    //-------Cartelera-----------
     app.get('/api/cartelera', getMovie)
     app.get('/api/cartelera/:id', getMovieById)
     app.post('/api/cartelera', addMovie)
     app.put('/api/cartelera/:id', editMovie)
     app.delete('/api/cartelera/:id', deleteMovie)
+    //------HandleError----------
     app.use(pageNotFound)
     app.use(handleError)
 }
